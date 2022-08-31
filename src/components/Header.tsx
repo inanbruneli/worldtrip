a9da6d2192bffa2d03407845ddec81a02ecf2f8d
inanbruneli-home <58962462+inanbruneli@users.noreply.github.com>
2022-08-30 23:12:04 -0300

    config ambiente

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Header() {
+  return (
+    <Flex
+      as='header'
+      w='100%'
+      maxW={1480}
+      bg='black'
+      align='center'
+    >
+
+    </Flex>
+  )
+}
