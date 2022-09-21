a35bb7dbad8082fda3eab36ac46430e7b5680e10
inanbruneli-home <58962462+inanbruneli@users.noreply.github.com>
2022-09-21 15:33:00 -0300

    setinha

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,13 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
+import { useRouter } from 'next/router';
+import { AiOutlineLeft } from 'react-icons/ai';
+import NextLink from "next/link";
 
 export default function Header() {
+
+  const { asPath } = useRouter();
+  console.log(asPath)
+
   return (
     <Flex
       as='header'
@@ -8,6 +15,16 @@ export default function Header() {
       align='center'
       h='100px'
     >
+      {asPath != '/' && (
+        <NextLink href='../../' passHref>
+          <Box ml='10%' cursor='pointer'>
+            <AiOutlineLeft color='#47585B' fontSize='24px' fontWeight='bold' />
+          </Box>
+        </NextLink>
+      )
+      }
+
+
       <Image
         src="/images/logo.svg"
         alt='logo'
